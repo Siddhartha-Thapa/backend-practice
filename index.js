861001370b03ce0f363b1d72c77cdb1570a0c53d
Siddhartha Thapa <siddharthathapa001@gmail.com>
2025-07-15 23:24:16 +0545

    final changes

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.get('/',function(req,res){
     res.render("index");
 });
 
+app.get("/profile/:username/:age",function(req,res){
+    res.send(`welcome , ${req.params.username}`);
+    res.send(`your age is ${req.params.age}`);
+});
+
+
 app.listen(3000 , function(){
     console.log("its running boi");
 })
