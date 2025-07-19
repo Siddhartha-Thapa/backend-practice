b1e248c110aea0bad312e768de78a6ff3cc20ab6
Siddhartha Thapa <siddharthathapa001@gmail.com>
2025-07-19 23:22:50 +0545

    route created

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post('/create',function(req,res){
 });
 app.get('/file/:filename', function(req, res){
     fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
-        console.log(filedata);
+        res.render("show")
     } )
     
 })
