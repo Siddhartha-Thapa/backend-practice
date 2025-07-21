a158ebd53c0e0f8d1fb53c7b62bc96668b9249c8
Siddhartha Thapa <siddharthathapa001@gmail.com>
2025-07-21 23:37:15 +0545

    edit feature

--- index.js
+++ index.js
@@ -35,8 +35,6 @@ app.get('/edit/:filename', function(req,res){
 
 
 app.post('/edit', function(req,res){
-    fs.rename(`.files/${req.body.previous}`,`.files/${req.body.new}`,function(err){
-        res.redirect('/');
-    })
+    console.log(req.body)
 })
 app.listen(3000);
